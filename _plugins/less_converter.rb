dbcee75b4a330d568c3bc4dc7be1136008242b27
siavash <siavashserver@gmail.com>
2013-08-28 17:00:50 +0430

    Added support for less, dynamic stylesheet language and some post processing Jekyll plugins to minify generated files.
    - Adding support for less stylesheet language to make management of CSS stylesheets easier.
    - Adding support for jekyll-press to minify JavaScripts, HTML and CSS generated files as a post processing step to reduce page loading times.
    
    Following Ruby gems should be installed aside Jekyll in order to use new features: less, therubyracer and jekyll-press.

---
+++ _plugins/less_converter.rb
@@ -0,0 +1,41 @@
+# Jekyll automated less to css converter plugin
+#
+# Code taken from : https://gist.github.com/KBalderson/5689220
+#
+# Installation note : gem install less therubyracer
+# Usage note : Add two lines of --- at the top of .less file.
+
+module Jekyll
+  class LessConverter < Converter
+    safe true
+    priority :high
+    
+    def setup
+      return if @setup
+      require 'less'
+      @setup = true
+    rescue LoadError
+      STDERR.puts 'You are missing the library required for less. Please run:'
+      STDERR.puts ' $ [sudo] gem install less'
+      raise FatalException.new("Missing dependency: less")
+    end
+    
+    def matches(ext)
+      ext =~ /less|lcss/i
+    end
+    
+    def output_ext(ext)
+      ".css"
+    end
+    
+    def convert(content)
+      setup
+      begin
+        parser = Less::Parser.new
+        parser = parser.parse(content).to_css
+      rescue => e
+        puts "Less Exception: #{e.message}"
+      end
+    end
+  end
+end
